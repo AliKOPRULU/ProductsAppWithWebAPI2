c1df36ab51aa2983bfc0d18e81d9e4cae80887d8
Ali <Ali@DESKTOP-AK>
2016-12-21 16:20:53 +0300

    Model and Controller ok

---
+++ ProductsAppWithWebAPI2/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using ProductsAppWithWebAPI2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+
+namespace ProductsAppWithWebAPI2.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        Product[] products = new Product[]
+        {
+            new Product {Id=1,Name="Domates Çorbası",Category="Bakkal",Price=1 },
+            new Product {Id=2,Name="Yo-yo",Category="Oyuncak",Price=3.75M },
+            new Product {Id=3,Name="Çekiç",Category="Donanım",Price=16.99M }
+        };
+
+        public IEnumerable<Product> GetAllProducts()//IEnumerable implement Array, List, ArrayList, Hashtable. Used Foreach or while
+        {
+            return products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);            
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(products);
+        }
+    }
+}
